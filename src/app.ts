c8eae22d37140e6c6a8b9ed786f427639d0e5b66
gms-yu-mineo <yu-mineo@global-mobility-service.com>
2021-02-09 14:15:43 +0900

    express audit sample

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,19 @@
-console.log('hello world')
+import express from 'express'
+import onFinished from 'on-finished'
+const app = express();
+const port = 3000;
+
+app.use((_req, res, next) => {
+  onFinished(res,  (_err, res)  => {
+    console.log(res);
+  });
+  next();
+});
+
+app.get('/', (_req, res)  => {
+  res.status(200).json({result: 'SUCCESS!'});
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
